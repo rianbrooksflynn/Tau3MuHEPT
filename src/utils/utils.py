82d290ef221a958be7eb9b579d12c2141686cc4d
simon71701 <simon73@purdue.edu>
2024-09-16 14:15:43 -0400

    flexible saving for multiclustering

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -12,13 +12,22 @@ def load_checkpoint(model, optimizer, log_path, device):
     return start_epoch
 
 
-def save_checkpoint(model, optimizer, log_path, epoch, name='model'):
+def save_checkpoint(model, optimizer, log_path, epoch, criterion=None, name='model'):
     print(f'[INFO] Saving checkpoint to {log_path.name}')
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-    }, log_path / f'{name}.pt')
+    
+    if criterion != None:
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'criterion': criterion
+        }, log_path / f'{name}.pt')
+    else:
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict()
+        }, log_path / f'{name}.pt')
 
 
 def set_seed(seed):
